0c828dd75fbd7f3460d4183c2638fad86d41888b
Alexander Bogdanov <alexander@bogdanov.ru>
2025-01-19 14:22:00 +0300

    formatted code

--- src/main/kotlin/FunctionsToBenchmark.kt
+++ src/main/kotlin/FunctionsToBenchmark.kt
@@ -1,26 +1,35 @@
 package org.example
 
 fun codegen(json: MutableMap<String, Any?>) {
-    if ((json["a"] as String?)?.contains("a") == true && (json["b"] as String?)?.contains("b") == true) {
+    if (
+        (json["a"] as String?)?.contains("a") == true
+        && (json["b"] as String?)?.contains("b") == true
+    ) {
         json.computeIfAbsent("C") { 0 }
-        json.computeIfPresent("C") { _, v -> if (v is Int) v + 1 else v }
+        json.computeIfPresent("C") { _, v ->
+            if (v is Int) v + 1 else v
+        }
     }
 }
 
 fun interpretation(json: MutableMap<String, Any?>) {
-    if (INTERPRETATION_TREE.eval(json)) {
+    if (
+        INTERPRETATION_TREE.eval(json)
+    ) {
         json.computeIfAbsent("I") { 0 }
-        json.computeIfPresent("I") { _, v -> if (v is Int) v + 1 else v }
+        json.computeIfPresent("I") { _, v ->
+            if (v is Int) v + 1 else v
+        }
     }
 }
 
 private val INTERPRETATION_TREE = AndNode(
     ContainsFunctionNode(
-        StringExtractionNode { json -> (json["a"] as String?) ?: "" },
+        StringExtractionNode(extractField("a")),
         StringNode("a"),
     ),
     ContainsFunctionNode(
-        StringExtractionNode { json -> (json["b"] as String?) ?: "" },
+        StringExtractionNode(extractField("a")),
         StringNode("b"),
     )
 )
--- src/main/kotlin/Utils.kt
+++ src/main/kotlin/Utils.kt
@@ -7,3 +7,7 @@ private val CHAR_POOL = "abcdyadfngoibbrtqwerpoertyuixcvmn".toCharArray()
 fun randomStringByKotlinRandom(len: Int) = (1..len)
     .map { Random.nextInt(0, CHAR_POOL.size).let { CHAR_POOL[it] } }
     .joinToString("")
+
+fun extractField(field: String): (MutableMap<String, Any?>) -> String {
+    return { json: MutableMap<String, Any?> -> (json[field] as String?) ?: "" }
+}
